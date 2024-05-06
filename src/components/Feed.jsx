e183782dbe59433e30983172c6f4e5534258559a
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-06 20:44:35 +0400

    adding posts and miniprofile sections to Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,7 +6,11 @@ export default function Feed() {
     return (
         <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
             <section className='md:col-span-2'><Posts /></section>
-            <section className='hidden md:inline-grid md:cols-span-1'><MiniProfile /></section>
+            <section className='hidden md:inline-grid md:cols-span-1'>
+                <div className='fixed w-[380px]'>
+                    <MiniProfile />
+                </div>
+            </section>
         </main>
     )
 }
