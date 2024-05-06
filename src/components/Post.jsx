247add6e0eb930e37678dce50dda8c4ec9c26371
Gahramanova Laman <lemangahramanoval@gmail.com>
2024-05-06 23:52:54 +0400

    changing some designs of post card

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,23 +5,22 @@ import CommentList from './CommentList'
 export default function Post({ post }) {
     // console.log(post, 'post');
     return (
-        <div className='bg-white m-7 border rounded-md' >
-            <div className='flex item-center p-5 border-b border-gray-100'>
+        <div className='bg-white m-7 border rounded-md p-3 ' >
+            <div className='flex item-center p-5 border-b border-gray-100 '>
                 <img src={post.profileImg} alt={post.username}
                     className='h-12 rounded-full object-cover border p-1 mr-3'
                 />
                 <p className='flex-1 font-bold'>{post.username}</p>
                 <HiOutlineDotsVertical className='h-5 cursor-pointer' />
             </div>
-            <img src={post.image} alt={post.caption} className='object-cover w-full' />
+            <img src={post.image} alt={post.caption} className='object-cover w-full p-5' />
 
             <p className='p-5 truncate'>
                 <span className='font-bold mr-2'>{post.username}</span>
                 {post.caption}
             </p>
-            <hr/>
+            <hr />
             <CommentList post={post} />
-
         </div>
     )
 }
